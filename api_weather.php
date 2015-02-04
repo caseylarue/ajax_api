f5cc821d6be7de4c846b0c83761b6f961eee2ae5
Casey LaRue <casey.larue@gmail.com>
2015-02-04 12:35:45 -0800

    api demo

---
+++ api_weather.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+	<title>Weather API</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript">
+
+	</script>
+</head>
+<body>
+	<form action='http://api.worldweatheronline.com/free/v2/weather.ashx' method='get'>
+		<select name='q'>
+			<option value='94303'>Mountain View</option>
+			<option value='98005'>Seattle</option>
+			<option value='77005'>Houston</option>
+		</select>
+	</form>
+</body>
+</html>
