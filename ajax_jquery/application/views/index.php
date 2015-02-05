9d4bff8e32bff28b721b0a1ebb9b928f205791fd
Casey LaRue <casey.larue@gmail.com>
2015-02-04 20:22:54 -0800

    ajax jquery

---
+++ ajax_jquery/application/views/index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+	<title>Ajax_jQuery</title>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function(){
+			$('#mainForm').submit(function(){
+				$.post(
+					$('')
+					)
+
+				return false;
+			});
+		});
+	</script>
+</head>
+<body>
+	<form id='mainForm' action='/users/random' method='post'>
+		<button type='submit'>Via Ajax, grab information of a random person</button>
+	</form>
+	<div id='messages'>
+	</div>
+</body>
+</html>
